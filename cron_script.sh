52894c128e4b2d447db790ccd1ae6937217363d7
Ubuntu <ubuntu@ubuntu.fritz.box>
2021-10-31 21:13:59 +0100

    Cron Commit

--- cron_script.sh
+++ cron_script.sh
@@ -2,7 +2,7 @@
 cd ~/Python_Projects/degewo_monitor/
 #source usedobjenv/bin/activate
 ./monitor.py
-/home/ubuntu/Python_Projects/degewo_monitor/venv/bin/papermill scratchfile.ipynb scratchfile.ipynb
+#/home/ubuntu/Python_Projects/degewo_monitor/venv/bin/papermill scratchfile.ipynb scratchfile.ipynb
 git add .
 git commit -m "Cron Commit"
 git push origin main
